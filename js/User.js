89587496af6527236c8e8a0dd30f4c86430de5bd
Petar Jovanovic <pexxxuss@gmail.com>
2022-11-30 18:18:25 +0100

    Ready for deploy

---
+++ js/User.js
@@ -0,0 +1,90 @@
+class User {
+  user_id = '';
+  username = '';
+  email = '';
+  password = '';
+  api_url = 'https://6325afff70c3fa390f8cd1c4.mockapi.io';
+  create() {
+    let data = {
+      username: this.username,
+      email: this.email,
+      password: this.password,
+    };
+    data = JSON.stringify(data);
+    fetch(this.api_url + '/Users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: data,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        let session = new Session();
+        session.user_id = data.id;
+        session.startSession();
+        window.location.href = 'hexa.html';
+      });
+  }
+  async get(user_id) {
+    let api_url = this.api_url + '/Users/' + user_id;
+    let response = await fetch(api_url);
+    let data = await response.json();
+    return data;
+  }
+  edit() {
+    let data = {
+      username: this.username,
+      email: this.email,
+    };
+    data = JSON.stringify(data);
+    let session = new Session();
+    session_id = session.getSession();
+    fetch(this.api_url + '/Users/' + session_id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: data,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        window.location.href = 'hexa.html';
+      });
+  }
+  login() {
+    fetch(this.api_url + '/Users')
+      .then((response) => response.json())
+      .then((data) => {
+        let login_succesful = 0;
+        data.forEach((db_user) => {
+          if (
+            db_user.email === this.email &&
+            db_user.password === this.password
+          ) {
+            let session = new Session();
+            session.user_id = db_user.id;
+            session.startSession();
+            login_succesful = 1;
+            window.location.href = 'hexa.html';
+          }
+        });
+        if (login_succesful === 0) {
+          alert('Pogresan email ili password');
+        }
+      });
+  }
+  delete() {
+    let session = new Session();
+    session_id = session.getSession();
+    fetch(this.api_url + '/Users/' + session_id, {
+      method: 'DELETE',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        let session = new Session();
+        session.destroySession();
+        window.location.href = '/';
+      });
+  }
+}
